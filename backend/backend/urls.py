6f37712d72ec47a39ab2219c6da21779d6488c4b
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-10-27 17:41:31 +0100

    Refactor URL patterns to include separate endpoints for authentication and medical records

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('userauth.urls'))
+    path('api/auth/', include('userauth.urls')),
+    path('api/medical-record/', include('medical_record.urls')),
 ]
