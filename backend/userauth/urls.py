8b3ad3c5f4479d4c98bc80bf082b6970cd26087c
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-10-27 17:44:33 +0100

    Refactor authentication URLs in userauth/urls.py

--- backend/userauth/urls.py
+++ backend/userauth/urls.py
@@ -11,13 +11,12 @@ from .views import (RegistrationAPIView,
 
 urlpatterns = [
     # authentication endpoints
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('auth/user/register', RegistrationAPIView.as_view(), name='register'),
-    path('auth/user/verify-email', VerifyEmailAPIView.as_view(), name='verify_email'),
-    path('auth/user/resend-verify-email', ResendVerifyEmailAPIView.as_view(), name='resend_verify_email'),
-    path('auth/user/login', UserLogInAPIView.as_view(), name='login'),
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register/', RegistrationAPIView.as_view(), name='register'),
+    path('user/verify-email/', VerifyEmailAPIView.as_view(), name='verify_email'),
+    path('user/resend-verify-email/', ResendVerifyEmailAPIView.as_view(), name='resend_verify_email'),
+    path('user/login/', UserLogInAPIView.as_view(), name='login'),
     path('user/me/', UserAPIView.as_view(), name='user'),
 
 ]
