53b0326a780767de9a71916e6f9d6a9e6cd2acc8
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-11-28 17:34:08 +0100

    Register Doctor and Hospital models in admin interface

--- backend/userauth/admin.py
+++ backend/userauth/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Doctor, Hospital
 
 admin.site.register(User)
+admin.site.register(Doctor)
+admin.site.register(Hospital)
