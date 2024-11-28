a487552b48ce77c2e73b121ece70058538469508
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-11-28 17:34:28 +0100

    Add doctor and patient profile endpoints to user authentication URLs

--- backend/userauth/urls.py
+++ backend/userauth/urls.py
@@ -1,16 +1,20 @@
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView
+    TokenRefreshView,
 )
 from django.urls import path
-from .views import (RegistrationAPIView, 
-                    VerifyEmailAPIView, 
-                    ResendVerifyEmailAPIView, 
-                    UserAPIView,
-                    UserLogInAPIView)
+from .views import (
+    RegistrationAPIView,
+    VerifyEmailAPIView,
+    ResendVerifyEmailAPIView,
+    UserAPIView,
+    UserLogInAPIView,
+    DoctorProfileAPIView,
+    PatientProfileAPIView,
+)
 
 urlpatterns = [
-    # authentication endpoints
+    # Authentication endpoints
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/register/', RegistrationAPIView.as_view(), name='register'),
@@ -18,5 +22,8 @@ urlpatterns = [
     path('user/resend-verify-email/', ResendVerifyEmailAPIView.as_view(), name='resend_verify_email'),
     path('user/login/', UserLogInAPIView.as_view(), name='login'),
     path('user/me/', UserAPIView.as_view(), name='user'),
-
+    
+    # Additional profile endpoints
+    path('user/me/doctor/', DoctorProfileAPIView.as_view(), name='doctor_profile'),
+    path('user/me/patient/', PatientProfileAPIView.as_view(), name='patient_profile'),
 ]
